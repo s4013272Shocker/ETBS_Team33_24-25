c76cf584c284264bea6b6f32658b8c1c4364addb
s4013272Shocker <s4013272@lsbu.ac.uk>
2025-04-11 05:41:07 +0100

    React App created.

--- ETBS_Website/ETBS_App/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EtbsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ETBS_App'
--- ETBS_Website/ETBS_Website/settings.py
+++ ETBS_Website/ETBS_Website/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ETBS_Website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'etbs_reactapp/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'etbs_reactapp/build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
